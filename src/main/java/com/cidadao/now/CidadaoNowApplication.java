f40c3106f343870dc856805d6c830c32a61001e9
Maycon_Pereira <pmaycon63@gmail.com>
2025-05-20 16:26:30 -0300

    first commit of CidadaoNow

---
+++ src/main/java/com/cidadao/now/CidadaoNowApplication.java
@@ -0,0 +1,13 @@
+package com.cidadao.now;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CidadaoNowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CidadaoNowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cidadao/now/CidadaoNowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cidadao.now;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CidadaoNowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
